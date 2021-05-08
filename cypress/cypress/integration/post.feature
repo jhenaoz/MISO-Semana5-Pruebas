701828ff3e11e77c119380dbdc1ed77986fad58c
Zully Alarcon <zullyalarcon@gmail.com>
2021-05-08 16:18:38 -0500

    agregar nuevo caso de prueba

--- cypress/cypress/integration/post.feature
+++ cypress/cypress/integration/post.feature
@@ -7,3 +7,13 @@ Feature: Google Post Page
         Given I open ghost post page to create one
         When I login with "z.alarcon@uniandes.edu.co" and password "m4d1zus42302"
         Then I go to the post page
+        When I create a post with title "Post Test" and body "Cuerpo texto"
+    
+    @focus
+    Scenario: Publish a post
+        Given I open ghost post page in publish specific post
+        When I login with "z.alarcon@uniandes.edu.co" and password "m4d1zus42302"
+        Then I go to the post page in publish specific post 
+        When I change title with old text "Post Test" and new text "Post Test 2"
+        Then I see "Published" in the page
+
--- cypress/cypress/integration/post/post.js
+++ cypress/cypress/integration/post/post.js
@@ -1,7 +1,8 @@
 import { Given, Then, When } from "cypress-cucumber-preprocessor/steps";
 
-const url1 = 'http://localhost:2368/ghost/#/signin'
-const url2 = 'http://localhost:2368/ghost/#/post'
+const url1 = 'http://localhost:2369/ghost/#/signin'
+const url2 = 'http://localhost:2369/ghost/#/posts'
+const url3 = 'http://localhost:2369/ghost/#/editor/post/608ef84525477f5493343ead';
 
 Given('I open ghost post page to create one', () => {
     cy.visit(url1)
@@ -13,4 +14,25 @@ When(`I login with {string} and password {string}`, (username, password) => {
 
 Then(`I go to the post page`, () => {
     cy.visit(url2)
+})
+
+// I create a post with title "Post Test" and body "Cuerpo texto"
+When(`I create a post with {title} and role {body}`, (title, body) => {
+    cy.get('.view-actions').click();
+    cy.get('#ember1675').click().type(title, {force: true});
+    cy.get('#ember1676').select(body, {force: true});
+    cy.get('#ember1669').click();
+})
+
+Then(`I go to the post page`, () => {
+    cy.visit(url3)
+})
+
+// I change title with old text for new text "Post Test 2"
+When(`I change title with old text for new text {string}`, (newTitle) => {
+    cy.get('#ember1675').click().type(newTitle, {force: true});
+    cy.get('#ember1965').click();
+    cy.get('.gh-publishmenu-radio-content').contains('Set it live now').click();
+    cy.get('button > span').contains('Publish').click();
+    cy.get('#ember26 > article > div').contains('Published');
 })
