6a62e538e3abf341da1c605e8d8c67436c5529ac
Sebastian Noreña Marquez <s.norenam@uniandes.edu.co>
2021-05-13 15:41:35 -0500

    se cambian tags para que tome la informacion del archivo de configuracion

--- playwrigth/spec/tags/tags.spec.js
+++ playwrigth/spec/tags/tags.spec.js
@@ -1,13 +1,15 @@
 const playwright = require('playwright');
+const config = require('config');
+const util = require('util');
 const { Login } = require('../../src/login.page')
 const { Tags } = require('../../src/tags.page')
 const GhostAdminAPI = require('@tryghost/admin-api');
-const url = 'https://ghost3-3-0.herokuapp.com/ghost/#/signin';
-const tagsUrl = 'https://ghost3-3-0.herokuapp.com/ghost/#/tags/new';
-const tagsUrlBase = 'https://ghost3-3-0.herokuapp.com/ghost/#/tags';
-const internalTagUrl = 'https://ghost3-3-0.herokuapp.com/ghost/#/tags?type=internal';
-
-describe('Given I open ghost page Tags', () => {
+const url = `${config.url}`;
+const urlLogin = `${url}/#/signin`;
+const tagsUrl = `${url}/#/tags/new`;
+const tagsUrlBase = `${url}/#/tags`;
+const internalTagUrl = `${url}/#/tags?type=internal`;
+fdescribe('Given I open ghost page Tags', () => {
     let browser;
     let context;
     let page;
@@ -27,10 +29,10 @@ describe('Given I open ghost page Tags', () => {
     afterEach(async () => {
         await context.close();
         const api = new GhostAdminAPI({
-            url: 'https://ghost3-3-0.herokuapp.com',
+            url: config.urlApi,
             // Admin API key goes here
-            key: '6096f662bee550001c0d879b:d93bbe382d07538ea15f32fec8068324047b2f3cac813396ad0ac9faff2bea13',
-            version: 'v3'
+            key: config.key,
+            version: config.version
         });
         
         api.tags.browse({limit: 10})
@@ -45,19 +47,19 @@ describe('Given I open ghost page Tags', () => {
     describe('I create a tags with title "Tags Test" and body "Cuerpo texto"', () => {
         let nameTag = 'Tags Test';
         beforeEach(async () => {
-            await loginPage.login('admin-user@mailsac.com', 'Test4echo!');
-            let text = await page.textContent('.gh-user-email');
+            await loginPage.login(config.adminUser.email, config.adminUser.password);
+            let text = await page.textContent(config.tags.playwrigth.create.emailUser);
             await page.goto(tagsUrl);
             await page.screenshot({path: './tags-page.png'});
-            text = await page.textContent('#tag-name');
+            text = await page.textContent(config.tags.playwrigth.create.tagName);
             await tagsPage.create(nameTag);
             await page.goto(tagsUrlBase);
-            text = await page.click('text=\'Public tags\'');
+            text = await page.click(config.tags.playwrigth.create.publicTags);
             await page.screenshot({path: './tags-page-create.png'});
         });
 
         it('Then The tags "Tags Test" should be created', async () => {
-            const text_ = await page.textContent('.gh-tag-list-name');
+            const text_ = await page.textContent(config.tags.playwrigth.create.listTagsName);
             expect(text_).toContain(nameTag);
         });
     });
@@ -65,14 +67,14 @@ describe('Given I open ghost page Tags', () => {
     describe('I delete an internal tag a "Tags Test"', () => {
         let nameTag = 'Tags Test';
         beforeEach(async () => {
-            await loginPage.login('admin-user@mailsac.com', 'Test4echo!');
-            let text = await page.textContent('.gh-user-email');
+            await loginPage.login(config.adminUser.email, config.adminUser.password);
+            let text = await page.textContent(config.tags.playwrigth.create.emailUser);
             await page.goto(tagsUrl);
-            text = await page.textContent('#tag-name');
+            text = await page.textContent(config.tags.playwrigth.create.tagName);
             await tagsPage.create(nameTag);
             await page.goto(tagsUrlBase);
-            text = await page.click('text=\'Public tags\'');
-            text = await page.click('.gh-tag-list-name:has-text(\''+nameTag+'\')');
+            text = await page.click(config.tags.playwrigth.create.tagName);
+            text = await page.click(util.format(config.tags.playwrigth.delete.listTagsNameItem,nameTag));
             await tagsPage.delete(nameTag);
             await page.screenshot({path: './tags-page-delete.png'});
             await page.goto(tagsUrlBase);
@@ -81,8 +83,8 @@ describe('Given I open ghost page Tags', () => {
 
         it('The tag "Tags Test" should not be found', async () => {
             await page.goto(tagsUrlBase);
-            const text = await page.textContent('.gh-tags-placeholder');
-            expect(text).not.toContain('Tags Test');
+            const text = await page.textContent(config.tags.playwrigth.delete.tagsPlaceHolder);
+            expect(text).not.toContain(nameTag);
         });
     });
 
