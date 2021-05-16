ed4f1696898a702e5bc44b461673dfb308876dec
Juan Henao <jhenaoz93@gmail.com>
2021-05-16 17:52:36 -0500

    create tags images

--- cypress/cypress/integration/tags/tags.js
+++ cypress/cypress/integration/tags/tags.js
@@ -17,6 +17,7 @@ When(`I create a new tag with the name {string}`, (tagName) => {
   cy.get('.gh-btn').contains('New tag').click();
   cy.get('#tag-name').click({force: true}).type(tagName);
   cy.get('button').contains('Save').click();
+  cy.screenshot('Create Tag');
 });
 
 
@@ -40,6 +41,7 @@ Then('The internal tag {string} should not be present', () => {
   cy.reload()
   cy.visit(internalTagUrl);
   cy.get('h3').contains("You haven't created any internal tags yet!")
+  cy.screenshot('Tags Page');
 });
 
 Then('The tag {string} should not be present', () => {
